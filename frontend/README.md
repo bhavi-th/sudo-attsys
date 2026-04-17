2309e7f53b0706e78e5961a815ea87d90a3b7010
Bhavith S <bhaviths15@gmail.com>
2026-04-17 18:10:24 +0530

    Initial commit

---
+++ frontend/README.md
@@ -0,0 +1,27 @@
+# Frontend
+
+## Setup
+
+<ol>
+  <li>
+    <code>cd</code> into the frontend dir then run this command<br/>
+    <code>npm i</code>
+  </li><br/>
+  <li>
+    After all the installation is done. Run this command<br/>
+    <code>npm run dev</code>
+  </li><br/>
+  <li>
+    You will get an output like :<br/>
+      <code>VITE v7.2.6  ready in 224 ms
+➜ Local: http://localhost:5173/
+➜ Network: use --host to expose
+➜ press h + enter to show help</code>
+  </li>
+</ol>
+
+Note : When I say press <code>o</code> you don't have to keep on holding the key <code>o</code> and press <code>Enter</code>. You just have to press the key <code>o</code> once and then press the <code>Enter</code> key.
+
+Now press <code>o</code> and hit <code>Enter</code>. This will open the website in your browser.
+
+Press <code>q</code> and hit <code>Enter</code> to stop the vite / server / or whatever it's called.
---
+++ frontend/vite.config.js
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true, // This is mandatory to see the manifest in dev mode
+            },
+            manifest: {
+                id: '/',
+                name: 'ATTSYS 2.0',
+                short_name: 'ATTSYS 2.0',
+                description: 'Automated Attendance Ecosystem',
+                theme_color: '#000000',
+                background_color: '#000000',
+                display: 'standalone',
+                scope: '/',
+                start_url: '/',
+                icons: [
+                    {
+                        src: 'logo256.png', // No leading slash here
+                        sizes: '256x256',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'logo512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+                screenshots: [
+                    {
+                        src: 'screenshot-wide.png',
+
+                        sizes: '2560x1440',
+
+                        type: 'image/png',
+
+                        form_factor: 'wide',
+
+                        label: 'Desktop Dashboard',
+                    },
+
+                    {
+                        src: 'screenshot-mobile.png',
+
+                        sizes: '1500x2668',
+
+                        type: 'image/png',
+
+                        form_factor: 'narrow',
+
+                        label: 'Mobile View',
+                    },
+                ],
+            },
+        }),
+    ],
+});
